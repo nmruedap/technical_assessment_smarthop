db9dd64b9876a46c7eecc388ec1f41f195b9d9b2
nestormauro <nmruedap@unal.edu.co>
2020-11-22 09:08:11 -0500

    Learning time steps was increased. Evaluation environment was merged for learning and testing etages.

--- lunar_lander_dqn.py
+++ lunar_lander_dqn.py
@@ -6,27 +6,23 @@ from stable_baselines.common.vec_env import VecVideoRecorder, DummyVecEnv
 import os.path
 from os import path
 
+env = gym.make('LunarLander-v2')
+
 if path.isfile("l_lander_dqn.zip"):
     # Evaluation stage
     model = DQN.load("l_lander_dqn")
 
-    # Evaluation environment
-    eval_env = gym.make('LunarLander-v2')
-    mean_reward, std_reward = evaluate_policy(model, eval_env, n_eval_episodes=100)
-    obs = eval_env.reset()
+    obs = env.reset()
     for i in range(1000):
         action, _states = model.predict(obs)
-        obs, rewards, dones, info = eval_env.step(action)
-        eval_env.render()
+        obs, rewards, dones, info = env.step(action)
+        env.render()
 else:
-    # Learning stage
-    env = gym.make('LunarLander-v2')
-
     # DQN agent
     model = DQN('MlpPolicy', env, learning_rate=1e-3, prioritized_replay=True, verbose=1)
 
     # Train the agent
-    model.learn(total_timesteps=int(2e5))
+    model.learn(total_timesteps=int(5e5))
     model.save("l_lander_dqn")
     del model
 
@@ -34,10 +30,8 @@ else:
     model = DQN.load("l_lander_dqn")
 
     # Evaluation environment
-    eval_env = gym.make('LunarLander-v2')
-    mean_reward, std_reward = evaluate_policy(model, eval_env, n_eval_episodes=100)
-    obs = eval_env.reset()
+    obs = env.reset()
     for i in range(1000):
         action, _states = model.predict(obs)
-        obs, rewards, dones, info = eval_env.step(action)
-        eval_env.render()
+        obs, rewards, dones, info = env.step(action)
+        env.render()
